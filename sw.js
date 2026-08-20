0086adba56e05a66f45a419e0f082751720fae9c
rakisweb <tpbup-league20@yahoo.gr>
2026-08-20 09:39:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
 const CACHE_NAME = 'laf-v1';
-const assets = ['index.html', 'manifest.json'];
+const assets = [
+  'index.html',
+  'manifest.json'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
