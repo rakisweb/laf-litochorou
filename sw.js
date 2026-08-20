dbceed9627d0d7b1aa0414a50939490ba82847d0
rakisweb <tpbup-league20@yahoo.gr>
2026-08-20 09:30:17 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'laf-v1';
+const assets = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
